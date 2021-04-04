e64b293d0ac4e9b322f6223cb1c6c65a99fa8e4f
zdj <1450817025@qq.com>
2021-04-04 17:46:04 +0800

    first commit

---
+++ src/components/Main.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="main">
+        <div>
+            <div>应用</div>
+            <div>规划</div>
+            <div>我的 </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'Main',
+  props: {
+
+  }
+}
+</script>
+<style scoped>
+    .main{
+        width: 100%;
+        height: 100%;
+        background: url(../images/main-bg.jpg);
+        background-size: 100%;
+        position:fixed;
+        top: 0;
+        left: 0;
+    }
+</style>
